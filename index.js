fad45da8660cda4e56634810aff5888e8e67ffb5
Amit Kumar <amit.kumar4@tesco.com>
2019-07-23 01:03:49 +0530

    Used an external api to fetch data

--- index.js
+++ index.js
@@ -1,49 +1,75 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { RESTDataSource } = require('apollo-datasource-rest');
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+class PoliceDataAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = 'https://data.police.uk/api';
+  }
 
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
+  async getForces() {
+    return this.get(`forces`);
+  }
+  async getFoceInfoById(id){
+    return this.get(`forces/${id}`);
+  }
+
+  // willSendRequest(request) {
+  //   request.params.set('apiKey', this.context.apiKey);
+  //   request.params.set('country', this.context.country);
+  // }
+}
+
 const typeDefs = gql`
   # Comments in GraphQL are defined with the hash (#) symbol.
 
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
+  # This "Force" type can be used in other type declarations.
+  type Force {
+    id: String
+    name: String
   }
 
   # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
-    books: [Book]
+    Forces(id: String): [Force]
   }
 `;
 
 // Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
+// schema.  We'll retrieve forces from the "forces" array above.
+
 const resolvers = {
   Query: {
-    books: () => books,
+    Forces : async (_source, { id }, { dataSources }) => {
+      if (id) {
+        const force = await dataSources.policeDataAPI.getFoceInfoById(id);
+        return [force];
+      } else {
+        return await dataSources.policeDataAPI.getForces();
+      }
+    }
   },
 };
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      policeDataAPI: new PoliceDataAPI()
+    };
+  },
+  context: () => {
+    return {
+      // apiKey: 'b1aa965374644e34b41572a58a544715',
+      // country: 'us'
+    };
+  }
+});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
